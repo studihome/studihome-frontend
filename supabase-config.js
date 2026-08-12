28a15050938e24089e31624a14e5cb94f81a4930
studihome <studihome.mail@gmail.com>
2026-08-12 08:56:11 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,8 +1,5 @@
-const SUPABASE_URL = "https://hbfmhwwxbgidsnljupca.supabase.co";
+window.STUDIHOME_SUPABASE_URL =
+  "https://hbfmhwwxbgidsnljupca.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_publishable_134slHOJ_kcw5-kxDQDVaw_y1jFO4Lv";
-
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+window.STUDIHOME_SUPABASE_ANON_KEY =
+  "sb_publishable_134slHOJ_kcw5-kxDQDVaw_y1jFO4Lv";
