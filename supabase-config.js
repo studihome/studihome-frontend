963ec72cf8ab9a1624f13bb678341dfdda44c15e
studihome <studihome.mail@gmail.com>
2026-08-12 07:07:00 +0700

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://hbfmhwwxbgidsnljupca.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_134slHOJ_kcw5-kxDQDVaw_y1jFO4Lv";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
