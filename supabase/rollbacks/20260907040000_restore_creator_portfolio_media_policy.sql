bce9c37b1563c39e5283257eb659422f612632cf
Studihome <311398415+studihome@users.noreply.github.com>
2026-09-07 10:46:23 +0700

    fix(db): correct media policy regex escaping

--- supabase/rollbacks/20260907040000_restore_creator_portfolio_media_policy.sql
+++ supabase/rollbacks/20260907040000_restore_creator_portfolio_media_policy.sql
@@ -9,10 +9,10 @@ alter table public.creator_portfolios
   check (
     media_type = 'link' and media_url ~* '^https://'
     or media_type = 'image' and media_url ~* '^https://'
-    or media_type = 'youtube' and media_url ~* '^https://([^/]+\\.)?(youtube\\.com|youtu\\.be)(/|$)'
-    or media_type = 'drive' and media_url ~* '^https://([^/]+\\.)?(drive\\.google\\.com|docs\\.google\\.com)(/|$)'
-    or media_type = 'tiktok' and media_url ~* '^https://([^/]+\\.)?tiktok\\.com(/|$)'
-    or media_type = 'instagram' and media_url ~* '^https://([^/]+\\.)?instagram\\.com(/|$)'
+    or media_type = 'youtube' and media_url ~* '^https://([^/]+\.)?(youtube\.com|youtu\.be)(/|$)'
+    or media_type = 'drive' and media_url ~* '^https://([^/]+\.)?(drive\.google\.com|docs\.google\.com)(/|$)'
+    or media_type = 'tiktok' and media_url ~* '^https://([^/]+\.)?tiktok\.com(/|$)'
+    or media_type = 'instagram' and media_url ~* '^https://([^/]+\.)?instagram\.com(/|$)'
   );
 
 alter table public.creator_portfolios
@@ -34,10 +34,10 @@ alter table public.creator_portfolios
       when 'image' then media_url ~* '^https://'
       when 'video' then media_url ~* '^https://'
       when 'link' then media_url ~* '^https://'
-      when 'youtube' then media_url ~* '^https://(www\\.)?(youtube\\.com|youtu\\.be)/'
-      when 'drive' then media_url ~* '^https://(www\\.)?(drive\\.google\\.com|docs\\.google\\.com)/'
-      when 'tiktok' then media_url ~* '^https://(www\\.)?tiktok\\.com/'
-      when 'instagram' then media_url ~* '^https://(www\\.)?instagram\\.com/'
+      when 'youtube' then media_url ~* '^https://(www\.)?(youtube\.com|youtu\.be)/'
+      when 'drive' then media_url ~* '^https://(www\.)?(drive\.google\.com|docs\.google\.com)/'
+      when 'tiktok' then media_url ~* '^https://(www\.)?tiktok\.com/'
+      when 'instagram' then media_url ~* '^https://(www\.)?instagram\.com/'
       else false
     end
   ) not valid;
